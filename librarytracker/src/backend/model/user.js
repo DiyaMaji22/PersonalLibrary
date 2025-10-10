c6cbc71b785267738bf06814cbea128b20678e26
Diya Maji <diyamaji22@gmail.com>
2025-10-10 23:29:56 +0530

    Add basic layout for the Vintage Book Collection page with form and collection display

--- librarytracker/src/backend/model/user.js
+++ librarytracker/src/backend/model/user.js
@@ -7,16 +7,16 @@ const userSchema=new mongoose.Schema({
     email:{
         type:String,
         required:true,
-        // unique:true,
+        unique:true,
         lowercase:true,
     },
     password:{
         type:String,
         required:true,
-        minlength:8
+       
         
     }
 
 })
 const userModel=mongoose.model('user',userSchema);
-module.exports=userModel
+module.exports=userModel;
